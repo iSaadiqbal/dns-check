ed8664709eb619148eb2f8b18dc2335ccf3fc6e3
iSaadiqbal <139994436+iSaadiqbal@users.noreply.github.com>
2023-08-25 02:56:49 +0500

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -24,6 +24,8 @@ def send_email(subject, body, recipient_emails):
 dns_name = os.environ.get("RECORD_NAME", "")
 dns_type = os.environ.get("RECORD_TYPE", "")
 dns_content = os.environ.get("RECORD_CONTENT", "")
+trigger_count = os.environ.get("TRIGGER_COUNT", "")
+dns_comment = os.environ.get("DNS_COMMENT", "")
 ttl = os.environ.get("TTL", "")
 
 # Prepare DNS details string
@@ -33,7 +35,13 @@ Record Name: {dns_name}
 Record Type: {dns_type}
 Record Content: {dns_content}
 TTL: {ttl}
+Trigger Count: {trigger_count}
+Comment: {dns_comment}
 """
 
-recipient_emails = ["saad89.linux@gmail.com"]  # Add your recipient email addresses here
-send_email("DNS Record Details", dns_details, recipient_emails)
+# Get available DNS records
+available_records = "List of Available DNS Records:\n"
+# Replace with your code to retrieve and format available records using jq or other tools
+
+recipient_emails = ["recipient@example.com"]  # Add your recipient email addresses here
+send_email("DNS Record Details", dns_details + "\n" + available_records, recipient_emails)
